4b322af9e2367061a05311124e9022a8434079af
akaraj187 <akashnrajonwork@gmail.com>
2026-01-13 15:42:26 +0000

    Saved issues with labelling as hard if not GFI

--- pygit/pygithub.py
+++ pygit/pygithub.py
@@ -2,22 +2,29 @@ import csv
 from github import Github
 g=Github()
 repo = g.get_repo("ArduPilot/ardupilot")
+#i will write all issues by labelling non 'good first issues' as 'hard'
+all_issues = repo.get_issues(state='open')
 
-open_issues = repo.get_issues(state='open',labels=['good first issue'])
-
-with open('GF_issues.csv',mode='w',newline='',encoding='utf-8') as file:
+with open('all_issues.csv',mode='w',newline='',encoding='utf-8') as file:
     writer = csv.writer(file)
 
     writer.writerow(["sl.no","title","label","body"])
 
 
     i=1
-    for issue in open_issues:
-    
-        #print(f"{i} {issue.title}")
-        writer.writerow([i,issue.title,"good first issue",issue.body or " "])
-        i=i+1
-        if i > 10:
+    for issue in all_issues:
+          is_hard=True
+
+          for label in issue.labels:
+              if label.name =='good first issue':
+                writer.writerow([i,issue.title,"good first issue",issue.body or " "])
+                is_hard =False
+                break
+
+          if is_hard:
+            writer.writerow([i,issue.title,"Hard",issue.body or" "])
+          i=i+1
+          if i > 10:
            break
 
 print('Data Saved Succesfully')         
