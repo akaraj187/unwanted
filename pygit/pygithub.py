a0eccaade19582c3fcf097c7984810994b6805de
akaraj187 <akashnrajonwork@gmail.com>
2026-01-13 15:17:16 +0000

    stored the issues in a csv file

--- pygit/pygithub.py
+++ pygit/pygithub.py
@@ -1,11 +1,23 @@
+import csv
 from github import Github
 g=Github()
 repo = g.get_repo("ArduPilot/ardupilot")
+
 open_issues = repo.get_issues(state='open',labels=['good first issue'])
-i=1
-for issue in open_issues:
+
+with open('GF_issues.csv',mode='w',newline='',encoding='utf-8') as file:
+    writer = csv.writer(file)
+
+    writer.writerow(["sl.no","title","label","body"])
+
+
+    i=1
+    for issue in open_issues:
     
-      print(f"{i} {issue.title}")
-      i=i+1
-      if i > 10:
-         break
+        #print(f"{i} {issue.title}")
+        writer.writerow([i,issue.title,"good first issue",issue.body or " "])
+        i=i+1
+        if i > 10:
+           break
+
+print('Data Saved Succesfully')         
